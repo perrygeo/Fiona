05a0aa3398dc0e4ee97fa57d44694597065eb5cc
Matthew Perry <perrygeo@gmail.com>
2016-01-29 21:17:06 -0400

    add unique and try to parse floats from strings

--- fiona/fio/stats.py
+++ fiona/fio/stats.py
@@ -8,33 +8,46 @@ from cligj import features_in_arg
 
 @click.command(short_help="summary statistics")
 @features_in_arg
+@click.option("--unique", default=None)
 @click.pass_context
-def stats(ctx, features):
+def stats(ctx, features, unique):
     attr_names = set()
     values = defaultdict(list)
     for feature in features:
         prop = feature['properties']
         for key, value in prop.items():
             attr_names.add(key)
+            try:
+                value = int(value)
+            except ValueError:
+                try:
+                    value = float(value)
+                except ValueError:
+                    pass
+
             values[key].append(value)
 
-    headers = "attr min mean max n".split()
-    rows = []
-    for attr, values in values.items():
-        n = len(values)
-        try:
-            total = sum(values)
-            mean = total / float(n)
-        except TypeError:
-            total = ""
-            mean = ""
+    if unique:
+        for val in set(values[unique]):
+            click.echo(val)
+    else:
+        headers = "attr min mean max n".split()
+        rows = []
+        for attr, values in values.items():
+            n = len(values)
+            try:
+                total = sum(values)
+                mean = total / float(n)
+            except TypeError:
+                total = ""
+                mean = ""
 
-        row = (
-            attr,
-            min(values),
-            mean,
-            max(values),
-            n)
-        rows.append(row)
+            row = (
+                attr,
+                min(values),
+                mean,
+                max(values),
+                n)
+            rows.append(row)
 
-    print(tabulate(rows, headers=headers))
+        print(tabulate(rows, headers=headers))
